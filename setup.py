de961fb1e5396591da5717bf0788385de6ca6941
Júlio César Batista <julio.batista@outlook.com>
2019-05-22 17:25:42 -0300

    Packaging

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='spider-feeder',
+    version='0.1.0',
+    url='https://github.com/ejulio/spider-feeder',
+    author='Júlio César Batista',
+    author_email='julio@scrapinghub.com',
+    description='spider-feeder is a library to help loading inputs to scrapy spiders.',
+    long_description='spider-feeder is a library to help loading inputs to scrapy spiders.',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+    tests_require=[],
+    extras_require={},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Scrapy',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)

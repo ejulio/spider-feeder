fa4e327ae6de6a61fd957c6a10b33352f8f6bafc
Júlio César Batista <julio.batista@outlook.com>
2019-07-29 17:53:43 -0300

    Adding a loader to send meta keys in requests

--- spider_feeder/loaders.py
+++ spider_feeder/loaders.py
@@ -1,16 +1,16 @@
 from urllib.parse import urlparse
 import logging
 
-from scrapy import signals
+from scrapy import Request, signals
 from scrapy.exceptions import NotConfigured
 from scrapy.utils.misc import load_object
 
 logger = logging.getLogger(__name__)
 
 
-class StartUrlsLoader:
-    '''Loads a set of input urls to `spider.start_urls`.
-    The URLs are loaded from `SPIDERFEEDER_INPUT_URI`.
+class BaseLoader:
+    '''Base loader for `start_urls` and `start_requests` loaders.
+    The data is loaded from `SPIDERFEEDER_INPUT_URI`.
     The given store is loaded according to the scheme in `SPIDERFEEDER_INPUT_URI`.
     Currently, the support is for local file system, Amazon AWS S3, and Scrapinghub Collections.
     The stores can be overriden or aggregated through `SPIDERFEEDER_STORES`.
@@ -29,7 +29,7 @@ class StartUrlsLoader:
     def from_crawler(cls, crawler):
         input_uri = crawler.settings.get('SPIDERFEEDER_INPUT_URI', None)
         if not input_uri:
-            raise NotConfigured('StartUrlsLoader requires SPIDERFEEDER_INPUT_URI setting.')
+            raise NotConfigured('Loader requires SPIDERFEEDER_INPUT_URI setting.')
 
         stores = cls.STORES
         stores = dict(stores, **crawler.settings.getdict('SPIDERFEEDER_STORES', {}))
@@ -46,10 +46,7 @@ class StartUrlsLoader:
     def spider_opened(self, spider):
         input_uri = self._get_formatted_input_uri(spider)
         store = self._get_store(input_uri)
-        spider.start_urls = [url for (url, _) in store]
-        n_urls = len(spider.start_urls)
-        logger.info(f'Loaded {n_urls} urls from {input_uri}.')
-        self._crawler.stats.set_value(f'spider_feeder/{spider.name}/url_count', n_urls)
+        self.set_spider_input_data(spider, store)
 
     def _get_formatted_input_uri(self, spider):
         params = {k: getattr(spider, k) for k in dir(spider)}
@@ -59,3 +56,37 @@ class StartUrlsLoader:
         parsed = urlparse(input_uri)
         store_cls = load_object(self._stores[parsed.scheme])
         return store_cls(input_uri, self._crawler.settings)
+
+    def set_spider_input_data(self, spider, store):
+        raise NotImplementedError()
+
+
+class StartUrlsLoader(BaseLoader):
+    '''Loader setting spider.start_urls. For more information, please refer to BaseLoader.'''
+
+    def set_spider_input_data(self, spider, store):
+        spider.start_urls = [url for (url, _) in store]
+        n_urls = len(spider.start_urls)
+        self._crawler.stats.set_value(f'spider_feeder/{spider.name}/url_count', n_urls)
+
+
+class StartRequestsLoader(BaseLoader):
+    '''Loader setting spider.start_requests. For more information, please refer to BaseLoader.'''
+
+    def set_spider_input_data(self, spider, store):
+        spider.start_requests = _StartRequestsLoader(self._crawler, spider, store)
+
+
+class _StartRequestsLoader:
+    def __init__(self, crawler, spider, store):
+        self._crawler = crawler
+        self._spider = spider
+        self._store = store
+
+    def __call__(self):
+        n_urls = 0
+        for (url, meta) in self._store:
+            yield Request(url, meta=meta)
+            n_urls += 1
+
+        self._crawler.stats.set_value(f'spider_feeder/{self._spider.name}/url_count', n_urls)
